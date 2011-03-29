5102bc1ae54cd51be0b5b5eb9d9b76b8deeb8e1b
Reid Beels <mail@reidbeels.com>
2011-03-28 20:33:49 -0700

    Improve sorty view helper to add a class to the currently sorted column link and only reset the order to :asc for non-current column links.

--- lib/hey_sorty/helpers.rb
+++ lib/hey_sorty/helpers.rb
@@ -2,15 +2,15 @@ module HeySortyHelpers
   def sorty(column, options = {})
     # Options
     options = { :label => column.to_s.humanize.titleize }.merge(options)
-    
+
     # Add params
     query = params.merge({
       :column => column,
-      :order => (params[:order].eql?('asc') ? 'desc' : 'asc')
+      :order => params[:column].eql?(column.to_s) ? (params[:order].eql?('asc') ? 'desc' : 'asc') : 'asc'
     })
-  
+
     # Build new query string
-    class_name = query[:column].eql?(column.to_s) ? query[:order] : 'asc'
+    class_name = params[:column].eql?(column.to_s) ? "current #{query[:order]}" : 'asc'
     link_to(options[:label], query, :class => class_name)
   end
 end
