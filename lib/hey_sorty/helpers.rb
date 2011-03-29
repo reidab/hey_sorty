ec9a47d2c8867b4943e2e1b23a7be020cf1ecf41
Reid Beels <mail@reidbeels.com>
2011-03-28 23:20:37 -0700

    Improved sorty view helper to reflect details about the default sort order. Ideally, this would be accessible from where it was set on the model without having to pass it again here...

--- lib/hey_sorty/helpers.rb
+++ lib/hey_sorty/helpers.rb
@@ -1,16 +1,36 @@
 module HeySortyHelpers
+
+  # Generates a link to sort the collection by the given column
+  #
+  # Options:
+  #   - :label => The text of the link, defaults to the humanized version of the column name
+  #   - :default_order => Direction to use when first sorting by this column, either :asc or :desc
+  #   - :is_default =>  Specifies that the given column has been set as the default on the model, 
+  #                     in order to properly assign the 'current' class on initial page load with
+  #                     no params given. Can be set to :asc or :desc to match the direction specified
+  #                     in the model.
+  #
   def sorty(column, options = {})
     # Options
-    options = { :label => column.to_s.humanize.titleize }.merge(options)
+    options = { :label => column.to_s.humanize.titleize,
+                :default_order => :asc }.merge(options)
+
+    if options[:is_default] && params[:column].nil? && params[:order].nil?
+      is_current = true
+      current_order = options[:is_default]
+    else
+      is_current = params[:column].eql?(column.to_s)
+      current_order = params[:order]
+    end
 
     # Add params
     query = params.merge({
       :column => column,
-      :order => params[:column].eql?(column.to_s) ? (params[:order].eql?('asc') ? 'desc' : 'asc') : 'asc'
+      :order => is_current ? (current_order.to_s.eql?('asc') ? 'desc' : 'asc') : options[:default_order]
     })
 
     # Build new query string
-    class_name = params[:column].eql?(column.to_s) ? "current #{query[:order]}" : 'asc'
+    class_name = is_current ? "current #{query[:order]}" : options[:default_order]
     link_to(options[:label], query, :class => class_name)
   end
 end
